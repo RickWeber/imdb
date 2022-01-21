633e7bc73c17829dad5c9ab40d01f596684cb380
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-01-21 10:21:49 -0500

    add gitignore and README

---
+++ README.md
@@ -0,0 +1,4 @@
+# IMDB project
+
+This is a project to practice SQL using the [IMDB](https://datasets.imdbws.com/)
+dataset. 
--- download_files.py
+++ download_files.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import pandas as pd
 import requests
 import gzip
-
 
 name_basics = "https://datasets.imdbws.com/name.basics.tsv.gz"
 title_akas = "https://datasets.imdbws.com/title.akas.tsv.gz"
@@ -10,3 +10,11 @@ title_crew = "https://datasets.imdbws.com/title.crew.tsv.gz"
 title_episode = "https://datasets.imdbws.com/title.episode.tsv.gz"
 title_principals = "https://datasets.imdbws.com/title.principals.tsv.gz"
 title_ratings = "https://datasets.imdbws.com/title.ratings.tsv.gz"
+tables = [name_basics, title_akas, title_basics, title_crew, title_episode, title_principals, title_ratings]
+
+for t in tables:
+    req = requests.get(t)
+    file_gz = gzip.decompress(req.text)
+    file_text = ''.join(map(chr, file_gz))
+    df = pd.read_csv(file_text, sep = "\t")
+    
