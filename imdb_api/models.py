56a4f550deb3a9af989d2329ed8c79c64ebe9a2e
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-02-18 11:52:44 -0500

    trying to rollback to before making and then changing models

--- imdb_api/models.py
+++ imdb_api/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
-class Item(models.Model):
+class Person(models.Model):
     last_update = models.DateTimeField('last update')
     data = models.JSONField('json data')
-
-class Person(Item):
     person_id = models.CharField(max_length=9)
     primaryName = models.CharField(max_length=50)
 
-class Title(Item):
+class Title(models.Model):
+    last_update = models.DateTimeField('last update')
+    data = models.JSONField('json data')
     title_id = models.CharField(max_length=9)
 
-class Episode(Item):
+class Episode(models.Model):
+    last_update = models.DateTimeField('last update')
+    data = models.JSONField('json data')
     episode_id = models.CharField(max_length=9)
     parent_id = models.ForeignKey(Title, on_delete=models.CASCADE)
-    #title_id = models.ForeignKey('Title', on_delete=models.CASCADE)
     season_number = models.IntegerField(default=1)
     episode_number = models.IntegerField(default=1)
